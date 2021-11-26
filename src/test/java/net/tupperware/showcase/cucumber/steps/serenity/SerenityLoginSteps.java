81483171963329e0b2c2a5c23c2983f902053494
rachana.devi <rachana.devi@irissoftware.com>
2021-11-26 12:19:20 +0530

    Commit Logger changes

--- src/test/java/net/tupperware/showcase/cucumber/steps/serenity/SerenityLoginSteps.java
+++ src/test/java/net/tupperware/showcase/cucumber/steps/serenity/SerenityLoginSteps.java
@@ -7,13 +7,16 @@ import net.thucydides.core.annotations.Step;
 import net.thucydides.core.steps.ScenarioSteps;
 import net.tupperware.showcase.cucumber.pages.BusinessPage;
 import net.tupperware.showcase.cucumber.pages.TupperLoginPage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SuppressWarnings("serial")
 public class SerenityLoginSteps extends ScenarioSteps {
- 
-public TupperLoginPage tupperloginpage;  
 
-		  
+public TupperLoginPage tupperloginpage;
+
+	Logger logger = LoggerFactory.getLogger(SerenityLoginSteps.class);
+
 		  /*This method is use to log in Tupperware consultant*/
 		  @Step("Login to the Tupperware Application")
 			public BusinessPage tupper_consultant_user_login(String userName, String password) {
@@ -21,7 +24,8 @@ public TupperLoginPage tupperloginpage;
 			    getDriver().manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 			    tupperloginpage.open();
 			    String envURL = getDriver().getCurrentUrl();
-			    System.out.println("The  get driver url is " + envURL);
+//			    System.out.println("The  get driver url is " + envURL);
+			  logger.info("Driver URL is: "+envURL);
 			    String newurl =envURL.concat("");
 			    try {
 					Thread.sleep(3000);
@@ -30,7 +34,8 @@ public TupperLoginPage tupperloginpage;
 					e.printStackTrace();
 				}
 			      getDriver().navigate().to(newurl);
-			      System.out.println("The new url of the envURL" + newurl);
+//			      System.out.println("The new url of the envURL" + newurl);
+			  logger.info("New URL is: "+newurl);
 				LearningPlatformConstants.environmentURL.set(envURL);
 				return tupperloginpage.tupper_user_login(userName, password);	
 			} 
